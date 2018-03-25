dcdeddaca4749a729d2407c761b46e05b3404ebb
hossein-amini <hosein_amini@ymail.com>
2018-03-25 21:02:53 +0430

    get es_textFormat attr in EsamTextView

--- esamlibrary/src/main/java/ir/esam/esamlibrary/EsTextView.java
+++ esamlibrary/src/main/java/ir/esam/esamlibrary/EsTextView.java
@@ -12,11 +12,18 @@ import android.util.AttributeSet;
 public class EsTextView extends android.support.v7.widget.AppCompatTextView {
 
     private int mFontFace;
+    private int mTextFormat;
 
+    //
     private static final int FONT_FACE_ANDROID_DEFAULT_FONT = 0;
     private static final int FONT_FACE_IRAN_SANS_LIGHT = 1;
     private static final int FONT_FACE_IRAN_SANS_ULTRA_LIGHT = 2;
     private static final int FONT_FACE_IRAN_SANS_NORMAL = 3;
+
+    //
+    private static final int TEXT_FORMAT_NORMAL = 0;
+    private static final int TEXT_FORMAT_CURRENCY = 1;
+    private static final int TEXT_FORMAT_CARD_NUMBER = 2;
 
     public EsTextView(Context context) {
         super(context);
@@ -30,6 +37,8 @@ public class EsTextView extends android.support.v7.widget.AppCompatTextView {
         try {
             mFontFace = typedArray.getInt(R.styleable.EsTextView_es_fontFace,
                     FONT_FACE_IRAN_SANS_NORMAL);
+            mTextFormat = typedArray.getInt(R.styleable.EsTextView_es_textFormat,
+                    TEXT_FORMAT_NORMAL);
         } finally {
             typedArray.recycle();
         }
