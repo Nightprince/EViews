5f17d2a92384a85260c0e80cc57f1fe7296a8645
hossein-amini <hosein_amini@ymail.com>
2018-03-25 21:00:18 +0430

    get es_fontFace attr for EsamTextView

---
+++ esamlibrary/src/main/java/ir/esam/esamlibrary/EsTextView.java
@@ -0,0 +1,42 @@
+package ir.esam.esamlibrary;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+/**
+ * Created by hosseinAmini.
+ * EsamViews | Copyrights 2018 Esam.ir Crop.
+ */
+public class EsTextView extends android.support.v7.widget.AppCompatTextView {
+
+    private int mFontFace;
+
+    private static final int FONT_FACE_ANDROID_DEFAULT_FONT = 0;
+    private static final int FONT_FACE_IRAN_SANS_LIGHT = 1;
+    private static final int FONT_FACE_IRAN_SANS_ULTRA_LIGHT = 2;
+    private static final int FONT_FACE_IRAN_SANS_NORMAL = 3;
+
+    public EsTextView(Context context) {
+        super(context);
+    }
+
+    public EsTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.EsTextView);
+
+        try {
+            mFontFace = typedArray.getInt(R.styleable.EsTextView_es_fontFace,
+                    FONT_FACE_IRAN_SANS_NORMAL);
+        } finally {
+            typedArray.recycle();
+        }
+
+    }
+
+    public EsTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
