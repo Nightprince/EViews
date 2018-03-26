d3b372e2800afceaf57686f02b7082559ed87b89
hossein-amini <hosein_amini@ymail.com>
2018-03-26 17:29:29 +0430

    Create base of EsImageView

---
+++ esamViewslibrary/src/main/java/ir/esam/esamlibrary/EsImageView.java
@@ -0,0 +1,74 @@
+package ir.esam.esamlibrary;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by hosseinAmini.
+ * EsamViews | Copyrights 2018 Esam.ir Crop.
+ */
+public class EsImageView extends android.support.v7.widget.AppCompatImageView {
+
+    private Drawable mPlaceholder;
+    private Drawable mErrorPlaceholder;
+
+    public EsImageView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public EsImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.EsImageView);
+
+        try {
+            mPlaceholder = typedArray.getDrawable(R.styleable.EsImageView_es_placeholder);
+            mErrorPlaceholder = typedArray.getDrawable(R.styleable.EsImageView_es_placeholder_error);
+        } finally {
+            typedArray.recycle();
+        }
+
+        initialize();
+    }
+
+    public EsImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize();
+    }
+
+    private void initialize() {
+
+        setPlaceholder(mPlaceholder);
+        setErrorPlaceholder(mErrorPlaceholder);
+    }
+
+    private void setPlaceholder(Drawable placeholder) {
+        if(placeholder == null) {
+            // Todo: set default placeholder into mPlaceholder
+        }
+        mPlaceholder = placeholder;
+    }
+
+    private void setErrorPlaceholder(Drawable errorPlaceholder) {
+        if(errorPlaceholder == null) {
+            // Todo: set default error placeholder into mErrorPlaceholder
+        }
+        mErrorPlaceholder = errorPlaceholder;
+    }
+
+    public void setImageFormUrl(String url) {
+        Picasso.get().load(url).placeholder(mPlaceholder).error(mErrorPlaceholder).into(this);
+    }
+
+    public void setImageFromFile(String uri) {
+        Picasso.get().load(uri).placeholder(mPlaceholder).error(mErrorPlaceholder).into(this);
+    }
+
+}
