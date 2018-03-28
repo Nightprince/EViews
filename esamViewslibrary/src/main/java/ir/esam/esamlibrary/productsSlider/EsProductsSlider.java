c573f7be94bc70cfb3ef82fda550542e3e75b9ee
hossein-amini <hosein_amini@ymail.com>
2018-03-28 11:29:40 +0430

    Inflate es_products_slider.xml into EsProductsSlider custom view

--- esamViewslibrary/src/main/java/ir/esam/esamlibrary/productsSlider/EsProductsSlider.java
+++ esamViewslibrary/src/main/java/ir/esam/esamlibrary/productsSlider/EsProductsSlider.java
@@ -1,11 +1,72 @@
 package ir.esam.esamlibrary.productsSlider;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import ir.esam.esamlibrary.EsTextView;
+import ir.esam.esamlibrary.R;
+
 /**
  * Created by hosseinAmini.
  * EsamViews | Copyrights 2018 Esam.ir Crop.
  */
-public class EsProductsSlider{
+public class EsProductsSlider extends FrameLayout {
 
+    private String mTitle;
+    private EsTextView mTxtTitle;
+    private RecyclerView mRecyclerProducts;
 
+    public EsProductsSlider(@NonNull Context context) {
+        super(context);
+        initialize();
+    }
 
+    public EsProductsSlider(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.EsProductsSlider);
+
+        try {
+            mTitle = typedArray.getString(R.styleable.EsProductsSlider_es_title);
+        } finally {
+            typedArray.recycle();
+        }
+
+        initialize();
+    }
+
+    public EsProductsSlider(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize();
+    }
+
+    private void initialize() {
+
+        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.es_product_slider,
+                this, true);
+
+        mTxtTitle = rootView.findViewById(R.id.txt_title);
+        mRecyclerProducts = rootView.findViewById(R.id.recycler_products);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.HORIZONTAL, true);
+
+        mRecyclerProducts.setHasFixedSize(true);
+        mRecyclerProducts.setLayoutManager(layoutManager);
+
+        setTitle(mTitle);
+    }
+
+    public void setTitle(@NonNull String title) {
+        mTitle = title;
+        mTxtTitle.setText(mTitle);
+    }
 }
---
+++ esamViewslibrary/src/main/java/ir/esam/esamlibrary/productsSlider/ProductSliderItem.java
@@ -0,0 +1,47 @@
+package ir.esam.esamlibrary.productsSlider;
+
+/**
+ * Created by hosseinAmini.
+ * EsamViews | Copyrights 2018 Esam.ir Crop.
+ */
+public class ProductSliderItem {
+    //Todo: This model class must populate from json by GSON
+    //Todo: This class properties must change and complete later
+
+    private int mId;
+    private String mImageUrl;
+    private String mTitle;
+    private String mPrice;
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setPrice(String price) {
+        mPrice = price;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public String getmImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmPrice() {
+        return mPrice;
+    }
+}
